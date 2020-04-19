7e9434ace28c52b689788e214d06ebf19c8757a3
alichaabane <33007728+alichaabane@users.noreply.github.com>
2020-04-19 23:28:57 +0200

    the class that will contain the principal method Main (to run the app)

---
+++ src/javamail/MailSender.java
@@ -0,0 +1,18 @@
+package javamail;
+
+/**
+ *
+ * @author AliChaabane
+ */
+public class MailSender {
+    
+    public static void main(String[] args) throws Exception{
+        //il faut desactiver la propriété de votre compte gmail (si elle est desactiver) :https://myaccount.google.com/lesssecureapps
+        //u must disable the security for less secure apps in ur gmail account to avoid problem of transport,via
+        // this link : :https://myaccount.google.com/lesssecureapps
+        
+        //the argument for this method is the recipient address
+        JavaMail.sendMail("alichaabane98@gmail.com");
+    }    
+    
+}
